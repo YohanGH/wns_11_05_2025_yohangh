49aa19bf70e3509d9c0bf4b77ed8407cc39b45d6
YohanGH <regnier.yohan@proton.me>
2025-05-12 10:26:26 +0200

    feat: creates types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,1 +1,25 @@
 // you can put your types here
+
+export type Country = {
+  code: string;
+  name: string;
+  emoji: string;
+  continent?: Continent;
+};
+
+export type Continent = {
+  code: string;
+  name: string;
+};
+
+export type CountriesQuery = {
+  countries: Country[];
+};
+
+export type CountryQuery = {
+  country: Country;
+};
+
+export type ContinentsQuery = {
+  continents: Continent[];
+};
